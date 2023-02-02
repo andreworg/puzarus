78930338072a67ad4d828a563eb8df122781c516
Luca Andreucci <andrew@andrew.org>
2023-02-02 17:17:11 +0100

    several renames

--- puzarus.py
+++ puzarus.py
@@ -38,7 +38,7 @@ puzzle_22026 = np.array([
 correspondence_22026 = {6: 'T', 9: 'I', 2: 'R', 5: 'O'}
 
 
-def display_puzzle(puzzle, correspondence):
+def display_puzzle(puzzle):
     """
     Display the puzzle with the numbers and letters
     """
@@ -47,8 +47,8 @@ def display_puzzle(puzzle, correspondence):
     for row in puzzle:
         print("          ", end='')
         for cell in row:
-            if cell in correspondence:
-                print("{:>2}".format(correspondence[cell]), end=' ')
+            if cell in matches:
+                print("{:>2}".format(matches[cell]), end=' ')
             else:
                 if cell != 0:
                     print("{:>2}".format(cell), end=' ')
@@ -60,31 +60,31 @@ def display_puzzle(puzzle, correspondence):
         print("Completed!\n")
 
 
-def add_correspondence(number, letter):
+def add_matches(number, letter):
     """
-    Add a new correspondence between a number and a letter
+    Add a new match between a number and a letter
     """
-    global correspondence
-    correspondence[number] = letter
-    print(f"{number} now corresponds to {letter}")
+    global matches
+    matches[number] = letter
+    print(f"{number} now matches {letter}")
 
 
-def remove_correspondence(letter):
+def remove_matches(letter):
     """
-    Remove a correspondence between a number and a letter
+    Remove a match between a number and a letter
     """
-    global correspondence
-    correspondence = {key:val for key, val in correspondence.items() if val != letter}
+    global matches
+    matches = {key:val for key, val in matches.items() if val != letter}
     print(f"{letter} no longer corresponds to any number")
 
 
 # begin
 
 puzzle = puzzle_22026
-correspondence_start = correspondence_22026
-correspondence = correspondence_start.copy()
+matches_start = correspondence_22026
+matches = matches_start.copy()
 
-display_puzzle(puzzle, correspondence)
+display_puzzle(puzzle)
 
 # User interaction section
 while True:
@@ -96,9 +96,9 @@ while True:
         cchain[0] = cchain[0].upper()
     # screen refresh
     if cchain[0] == "REFRESH":
-        display_puzzle(puzzle, correspondence)
+        display_puzzle(puzzle)
 
-    # add corresp
+    # add new match(es)
     elif cchain[0] == "A":
         try:
             for a in range(1, len(cchain), 2):
@@ -113,44 +113,53 @@ while True:
                     print("{} is not a single letter, skipping".format(current_letter))
                     continue
 
-                if current_number in correspondence:
-                    print("{} already corresponds to {}, skipping".format(current_number, correspondence[current_number]))
-                elif current_letter in correspondence.values():
-                    print("{} already corresponds to {}, skipping".format(current_letter, list(correspondence.keys())[list(correspondence.values()).index(current_letter)]))
+                if current_number in matches:
+                    print("{} already corresponds to {}, skipping".format(current_number, matches[current_number]))
+                elif current_letter in matches.values():
+                    print("{} already corresponds to {}, skipping".format(current_letter, list(matches.keys())[list(matches.values()).index(current_letter)]))
                 elif (current_number < 1) or (current_number > 26):
                     print("Number must be between 1 and 26.")
                 else:
-                    add_correspondence(current_number, current_letter)
+                    add_matches(current_number, current_letter)
         except Exception: # PEP 8 E722
             traceback.print_exc()
-        display_puzzle(puzzle, correspondence)
+        display_puzzle(puzzle)
 
+    # remove match(es)
     elif cchain[0] == "R":
         try:
             for a in range(1, len(cchain)):
                 current_letter = cchain[a].upper()
                 if (not current_letter.isalpha() or (len(current_letter) > 1)):
                     print("{} is not a single letter, skipping".format(cchain[a]))
-                elif current_letter not in correspondence.values():
+                elif current_letter not in matches.values():
                     print("{} has no correspondence, skipping".format(current_letter))
                 else:
-                    remove_correspondence(current_letter)
+                    remove_matches(current_letter)
         except Exception:
             traceback.print_exc()
-        display_puzzle(puzzle, correspondence)
+        display_puzzle(puzzle)
+
+    # print current matches
     elif cchain[0] == "P":
         print()
-        print(correspondence)
+        print(matches)
         print()
-        for key in correspondence:
-            print("{} {} ".format(key, correspondence[key]), end='')
+        for key in matches:
+            print("{} {} ".format(key, matches[key]), end='')
         print("\n\n")
+
+    # revert to initial matches (start over)
     elif cchain[0] == "X":
-        correspondence = correspondence_start.copy()
-        display_puzzle(puzzle, correspondence)
+        matches = matches_start.copy()
+        display_puzzle(puzzle)
+
+    # quit
     elif cchain[0] == "Q":
         print("Bye!")
         sys.exit(0)
+
+    # help
     elif cchain[0] == "H":
         print('\n'
               '\n'
@@ -163,5 +172,7 @@ while True:
               '                  X: reset corresponcences\n'
               '                  H: this text\n'
               '\n')
+
+    # what?
     else:
         print("Invalid command")
